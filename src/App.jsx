77ace809d34bb5b9fe2691f321d86370c07c4bba
Amr <amralkomyinfo@gmail.com>
2025-11-19 16:23:23 +0200

    updated apislice.js and search now wait to be redirected to result page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 // import Search from "./components/Search"
 // import Mission2 from "./components/mission2"
 // import Sales from "./components/sales";
-import Sales from "./components/mission2";
+import Sales from "./components/search";
 
 function App() {
   return (
