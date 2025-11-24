2a736c0d8a7f644f7a88608bdb0a8eaf732af0dc
OMAR-A-M-A <oa5252842@gmail.com>
2025-11-24 19:47:56 +0200

    refresh

--- src/Service/Service.jsx
+++ src/Service/Service.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import ServiceCard from './ServicesCard'; 
+import { servicesData } from './ServicesData';
 function Service() {
   return (
-    <div>
-      Service
+    <div className="py-5 bg-light"> 
+      <div className="container">
+        <div className="row g-4">
+          
+          {servicesData.map((item) => (
+            <div key={item.id} className="col-lg-6">
+              <ServiceCard 
+                icon={item.icon}
+                title={item.title}
+                description={item.description}
+              />
+            </div>
+          ))}
+
+        </div>
+      </div>
     </div>
   )
 }
