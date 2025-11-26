5bb865fcdc31234b92df0148af3ca671865e8372
Eslam6104 <xedosedo12345@gmail.com>
2025-11-27 01:26:30 +0200

    card section center

--- src/Components/CardSection/CardSection.jsx
+++ src/Components/CardSection/CardSection.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import Card from "../Card/Card.jsx";
 import { useGetProductsQuery } from "../../Store/apislice.js";
 function CardSection({ limit }) {
-  const { data: products = [], isLoading, isError } = useGetProductsQuery();
-
+  const { data: products, isLoading, isError } = useGetProductsQuery();
   if (isLoading) return <h4>Loading...</h4>;
   if (isError) return <h4>Error loading products</h4>;
-  // If limit exists, slice. If not, use full list.
+  if (!products) return null;
   const visibleProducts = limit ? products.slice(0, limit) : products;
-
   return (
     <div className="container py-4">
-      <div className="row">
-        {visibleProducts.map((item) => (
-          <div className="col-3 mb-4" key={item.id}>
+      <div className="row justify-content-center">
+        {visibleProducts.map(item => (
+          <div
+            className="col-12 col-sm-8 col-md-6 col-lg-4 col-xl-3 mb-4 d-flex "
+            key={item.id}
+          >
             <Card
               title={item.title}
               description={item.description}
